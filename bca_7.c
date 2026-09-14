3a030c6ef72b382338550d8f2783e80c95b72e73
ANKITA PRASAD <ankitaprasad2799@gmail.com>
2026-09-14 12:04:38 +0530

    Add files via upload

---
+++ bca_7.c
@@ -0,0 +1,48 @@
+#include<stdio.h>
+int main()
+{
+    int  num = 20;
+
+    printf("value of num :%d\n",num);
+    // int sum;
+    // sum = num +2;
+    // printf("value of num :%d\n",sum);  
+    // int sub;
+    // sub = sum -2;
+    // printf("value of num :%d\n",sub);
+
+    num = num +1;
+    printf("add :%d\n",num);  
+
+    num = num -1;
+    printf("sub :%d\n",num);  
+
+    num+=5;
+    printf("add the number 5 :%d\n",num);  
+
+
+
+
+
+
+    // num = num +1;
+    // // printf("value of num :%d\n",num);
+    // printf("add 1 number :%d\n",num);
+
+    // printf("add 1 number :%d\n",num +1);
+    // int add ;
+    // add=num + 1;
+    // printf("add 1 number :%d\n",add);
+
+    // int sub;
+    // sub =add -1;
+    // printf("sub 1 number :%d\n",sub);
+
+
+
+
+
+
+    
+    return 0;
+}
