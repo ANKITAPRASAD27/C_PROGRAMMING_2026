8ada9a12e3ca1fad91c35ebed76e93c072696133
ANKITA PRASAD <ankitaprasad2799@gmail.com>
2026-09-16 22:34:42 +0530

    Add files via upload

---
+++ bca_8.c
@@ -0,0 +1,31 @@
+#include<stdio.h>
+int main()
+{
+    // long long int a = 100000000000;
+ 
+    // printf(" value of a:%lld\n",a);
+    // printf("size of long: %zu\n", sizeof(long long));
+
+    // short int age = 25;
+    // short a=26;
+    // printf("age :%d\n", age);
+    
+    // printf("a :%d\n", a);
+    // printf("size of short: %zu\n", sizeof(short));
+    
+    // signed int age = -25;
+    // printf("age :%d\n", age);
+    unsigned int a = 10u;
+    printf("a :%u\n", a);
+
+
+//   char c='A';
+//     printf("ascci value :%d\n", c);
+ 
+
+
+
+
+    return 0;
+   
+}
