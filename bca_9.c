9ad6aeddd7db32155003def747313cbb419b0a7a
ANKITA PRASAD <ankitaprasad2799@gmail.com>
2026-09-16 11:44:01 +0530

    Add files via upload

---
+++ bca_9.c
@@ -0,0 +1,71 @@
+#include<stdio.h>
+int main()
+{
+    // int a =10;
+
+    // a=a+1;
+    // a+=1;
+    // a++;//postfix->increament
+    // a--;// postfix->decreament
+    // varible ke bad ++ postfix
+    // ++a;//prefix->increament
+    // --a;// prefix->decreament
+    // printf("%d",a);
+//  int a=10;
+//  int b=a++;
+// printf("a :%d\n",a);
+// printf("b :%d\n",b);
+
+
+// int x = 20;
+// printf("x: %d\n",x);
+// printf("increament using postfix : %d\n",x++);
+// // int y=x;
+// int y = x++;
+// printf("y : %d\n",y);
+
+
+
+// int a=20;
+// printf("a:%d\n",a);
+// printf("increament using pretfix : %d\n",++a);
+// int b=++a;
+// printf("b : %d\n",b);
+
+// int x =10;
+// int y =++x;
+// printf("x:%d\n",x);
+// printf("y :%d\n",y);
+
+// int a =10;
+// int b = a++;
+// printf("a:%d\n",a);
+// printf("b :%d\n",b);
+
+// int a = 5;
+// a++;
+// printf("%d", a);
+
+// int a = 5;
+// int b = ++a;
+// printf("%d %d", a, b);
+
+int a =10;
+printf("a:%d\n",a);
+// printf("increament :%d\n",a++);
+int y;
+y=a++;
+printf("y: %d\n",y);
+
+int b =10;
+printf("b:%d\n",b);
+printf("increament :%d\n",++b);
+int v;
+v=++b;
+printf("y: %d\n",v);
+
+
+return 0;
+
+   
+}
