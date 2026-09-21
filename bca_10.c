8ca98dcc185e3ad3dd93350fe747bc764d8fd209
ANKITA PRASAD <ankitaprasad2799@gmail.com>
2026-09-21 20:06:26 +0530

    Add files via upload

---
+++ bca_10.c
@@ -0,0 +1,44 @@
+#include <stdio.h>
+int main()
+{
+    int a = 10;
+    printf("a:%d\n",a);
+    printf("a:%d\n",a++);
+    printf("a:%d\n",a);
+
+
+  
+}
+// int x = 10;
+// int y = x--  -  - -x;
+// printf("%d, %d", x, y);
+// return 0;
+// }
+// #include <stdio.h>
+// int main() {
+// int a = 5;
+// int b = a++ + ++a;
+// printf("%d", b);
+// return 0;
+// };
+
+// #include <stdio.h>
+// int main() 
+// {
+//     int x = 5;
+//     int y = ++x * x++;
+//     printf("x = %d, y = %d\n", x, y);
+//     return 0;
+// }
+
+// #include <stdio.h>
+// int main() {
+// int a = 0, b = 10, c = 20;
+// if (a++ && ++b) {
+// c += 5;
+// }
+// printf("a = %d, b = %d, c = %d\n", a, b, c);
+// return 0;
+// }
+
+
